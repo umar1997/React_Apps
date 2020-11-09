e56a4507ab5fccb70a47e26095e4f233e25aa332
Umar Salman <umar1997@users.noreply.github.com>
2020-11-09 23:09:17 +0500

    Boiler Plate Commit

---
+++ Boiler Plate/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import 'normalize.css/normalize.css'; // Same stuff for different browsers
+import './styles/styles.scss';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
